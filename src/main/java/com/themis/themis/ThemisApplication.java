7154d1ff01b9347ff897b7dbfed9a6cb81cd5d58
danieljr24 <daniel.cjrl.24@gmail.com>
2025-04-10 11:59:29 -0500

    primer commit themis

---
+++ src/main/java/com/themis/themis/ThemisApplication.java
@@ -0,0 +1,13 @@
+package com.themis.themis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThemisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThemisApplication.class, args);
+	}
+
+}
